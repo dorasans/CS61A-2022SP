8a348fcb84b41b90c576fc81e79d648b15671173
dorasans <sevenzyx2000@126.com>
2022-08-22 17:29:43 +0800

    disc04 loading...

--- disc/disc04/disc04.py
+++ disc/disc04/disc04.py
@@ -24,5 +24,16 @@ def count_k(n, k):
     1
     """
     "*** YOUR CODE HERE ***"
-    for step in range(1,k):
+    if n==0:
+        return 1
+    elif n<0:
+        return 0
+    else:
+        total = 0
+        i=1
+        while i<=k:
+            total+=count_k(n-i,k)
+            i+=1
+        return total
+
         
